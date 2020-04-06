455197194666fb841a53ada1faade864e3ed1e8e
Parker Burchett <parkerburchett@gmail.com>
2020-04-06 16:29:56 -0700

    This is the start
    I don't know an easy way to create a text file that looks like this
    a,A, 00
    b,B,01
    ...
    
    I need this to make the first Array of Encoders

---
+++ CaeserEncrypter.java
@@ -0,0 +1,23 @@
+/* simple Ceaser Cipher. 
+This program reads in a text file and creates a new text file that is that text file ecrypted
+
+each of the normal letters with an other moved a set number down the alphebet.
+
+This ignores case and puncation. All things are converted into capitals.
+
+
+method:
+
+1. read in file. 
+2. create a new text file and replace every character with its associted number eg( a or A = 00 .. z or Z = 25). This is the intermedite file
+3. For each number add x (where x is the code chosen) and create a new file where all of the numbers are +5 %26. This is the almost encoded file
+4. take encoded file and crate a new file where each of the numbers are replacted with an assocated letter. eg This is the fully encorded file
+
+
+5. You need an array of all the Encoder objects of all the letters
+*/
+
+public class CaeserEncrypter
+{
+   
+}
---
+++ Encoder.java
@@ -0,0 +1,14 @@
+public class Encoder
+{
+   public String lowerCase;
+   public String upperCase;
+   public String number;// this needs to be 2 diget int always
+   
+   public Encoder(String lower, String upper, String num)
+   {
+      this.lowerCase = lower;
+      this.upperCase = upper;
+      this.number = num;
+   }
+   
+}
