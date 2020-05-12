c6f8d0f6ac16ae94450d47b6ac0033dfedd13b2b
Parker Burchett <parkerburchett@gmail.com>
2020-05-12 11:12:28 -0700

    General Cleaning

--- CryptographyThings/OtherSolvers/KnownAffineSolver.java
+++ CryptographyThings/OtherSolvers/KnownAffineSolver.java
@@ -1,14 +1,18 @@
-public class Prog3Solver
+public class KnownAffineSolver
 {
 // this works since there are only 26^2 possible answers. I think maybe
    public static void main(String[] args)
    {
-      int i =8;
-      int n= 13;
-      int v =21;
-      int s =18;
-      int TrueA =0;
-      int TrueB=0;
+      // This finds A and B when SV converst into IN
+      
+       int s =1;
+       int i =4;
+       
+       int v =12;
+       int n= 7;
+      
+      int TrueA = 0;
+      int TrueB = 0;
       for(int a =0; a<26; a++)
       {
          for(int b = 0; b<26; b++)
@@ -18,20 +22,11 @@ public class Prog3Solver
                // Calculates  the proper a and b values for the given letter pairs 
                TrueA =a;
                TrueB =b;
+               System.out.println(TrueA);
+               System.out.println(TrueB);
             }
          }
       }
-      int h = 7;
-      int g = 6;
-      int k = 10;
-      int a = 0;
       
-      int[] ar =  {s,v,h,g,k,a};
-      
-      for(int x: ar)
-      {
-         int ans =(TrueA*x + TrueB)%26;
-         System.out.println(ans);
-      }
    }
 }
